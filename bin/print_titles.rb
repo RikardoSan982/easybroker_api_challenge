0f2b52b8842873b208f9b6c3788c0097ddca68c5
RikardoSan982 <rikardosan3@gmail.com>
2026-01-04 23:16:41 -0600

    Update print_titles.rb

--- bin/print_titles.rb
+++ bin/print_titles.rb
@@ -3,10 +3,7 @@
 require_relative "../lib/easybroker/client"
 require_relative "../lib/easybroker/properties"
 
-api_key = ENV.fetch("EASYBROKER_API_KEY") do
-  warn "Falta la variable EASYBROKER_API_KEY"
-  exit 1
-end
+api_key = ENV.fetch("EASYBROKER_API_KEY") { abort("Falta EASYBROKER_API_KEY") }
 
 client = EasyBroker::Client.new(api_key: api_key)
 properties = EasyBroker::Properties.new(client: client)
